814c7b6efc5ea78916a0abe4dedf3a13e4bec3af
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-31 15:13:06 +0100

    added portfolio bio content and bio component to portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import BioArrays from "../utils/BioArrays"
+import Bio from "../components/Bio"
 
 const Portfolio = () => {
+  const [portfolioBio] = useState(BioArrays.PortfolioBio)
   return (
-    <h1>Portfolio Page</h1>
+    <section className="content">
+    <Bio bioElements={portfolioBio} />
+    </section>
   )
 }
 
