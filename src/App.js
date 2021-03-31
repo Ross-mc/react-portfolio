014224c2b0853e37764c3f5dd0572820918321de
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-31 14:15:47 +0100

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route} from "react-router-dom"
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 
 
+
 const App = () => {
   return (
     <>
-    <Router>
-      <NavBar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/portfolio" component={Portfolio} />
-    </Router>
-    <div className="bg-img" />
+      <div className="bg-img" />
+      <div className ="my-container">
+      <Router>
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+      </Router>
+      <Footer />
+      </div>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="sticky-footer">
+      <div className="socialImages">
+        <a href="https://codepen.io/ross-mc">
+          <i className="fab fa-codepen fa-2x"></i>
+        </a>
+        <a href="https://github.com/Ross-mc">
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/ross-mcintyre-54a13787/">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <a href="mailto:rossmci90@hotmail.co.uk">
+          <i className="fas fa-at"></i>
+        </a>
+        <p className="footer-text small">&copy; Ross McIntyre 2020</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
