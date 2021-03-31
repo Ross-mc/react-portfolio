5ffb85048cb546e7c48cd98eaefaf65e7ecc458f
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-31 22:50:37 +0100

    added classname to portfolio bio

--- src/utils/BioArrays.js
+++ src/utils/BioArrays.js
@@ -4,10 +4,10 @@ const BioArrays = {
         <p className="responsive-text">web developer <span className="blue-text">/</span> software engineer</p>
     ],
     PortfolioBio: [
-        <p class="responsive-text">My name is <span class="blue-text">Ross McIntyre</span> and I am a web developer and software engineer at BJSS in Manchester</p>,
-        <p class="responsive-text">I am a Graduate of the <a class="blue-text inline-link" href="https://bootcamp.manchester.ac.uk/">University of Manchester Coding Bootcamp</a></p>,
-        <p class="responsive-text">I am a Full Stack Web Developer, primary using the MERN stack. I enjoy creating dynamic and intuitive front end UIs with React.js and my back end experience is primary Node.js/Express.js. </p>,
-        <p class="responsive-text">Below is a showcase of some of the projects I have been working on.</p>
+        <p className="responsive-text">My name is <span className="blue-text">Ross McIntyre</span> and I am a web developer and software engineer at BJSS in Manchester</p>,
+        <p className="responsive-text">I am a Graduate of the <a className="blue-text inline-link" href="https://bootcamp.manchester.ac.uk/">University of Manchester Coding Bootcamp</a></p>,
+        <p className="responsive-text">I am a Full Stack Web Developer, primary using the MERN stack. I enjoy creating dynamic and intuitive front end UIs with React.js and my back end experience is primary Node.js/Express.js. </p>,
+        <p className="responsive-text">Below is a showcase of some of the projects I have been working on.</p>
     ]
 }
 
