e142a6bccbf291ef54084044e69d9273335fdc8f
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-03-31 22:54:09 +0100

    added key to bio elements, removed some comments and console logs

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,24 +1,6 @@
 import React from "react";
 
-// HomeBio: [
-//     [
-//       { type: p, text: "My name is " },
-//       { type: span, text: "Ross McIntyre" },
-//     ],
-//     [
-//         {type: p, text: "web developer "},
-//         {type: span, text: "/"},
-//         {type: p, text: "software engineer"}
-//     ]
-//   ],
-
-{
-  /* <p className="responsive-text"
-            </p> */
-}
-
 const Bio = ({ bioElements }) => {
-    console.log(bioElements)
   return (
     <div className="bio">
         {bioElements}
--- src/utils/BioArrays.js
+++ src/utils/BioArrays.js
@@ -1,13 +1,13 @@
 const BioArrays = {
     HomeBio: [
-        <p className="responsive-text">My name is <span className="blue-text">Ross McIntyre</span></p>,
-        <p className="responsive-text">web developer <span className="blue-text">/</span> software engineer</p>
+        <p className="responsive-text" key="first">My name is <span className="blue-text">Ross McIntyre</span></p>,
+        <p className="responsive-text"  key="second">web developer <span className="blue-text">/</span> software engineer</p>
     ],
     PortfolioBio: [
-        <p className="responsive-text">My name is <span className="blue-text">Ross McIntyre</span> and I am a web developer and software engineer at BJSS in Manchester</p>,
-        <p className="responsive-text">I am a Graduate of the <a className="blue-text inline-link" href="https://bootcamp.manchester.ac.uk/">University of Manchester Coding Bootcamp</a></p>,
-        <p className="responsive-text">I am a Full Stack Web Developer, primary using the MERN stack. I enjoy creating dynamic and intuitive front end UIs with React.js and my back end experience is primary Node.js/Express.js. </p>,
-        <p className="responsive-text">Below is a showcase of some of the projects I have been working on.</p>
+        <p className="responsive-text"  key="first">My name is <span className="blue-text">Ross McIntyre</span> and I am a web developer and software engineer at BJSS in Manchester</p>,
+        <p className="responsive-text"  key="second">I am a Graduate of the <a className="blue-text inline-link" href="https://bootcamp.manchester.ac.uk/">University of Manchester Coding Bootcamp</a></p>,
+        <p className="responsive-text"  key="third">I am a Full Stack Web Developer, primary using the MERN stack. I enjoy creating dynamic and intuitive front end UIs with React.js and my back end experience is primary Node.js/Express.js. </p>,
+        <p className="responsive-text"  key="fourth">Below is a showcase of some of the projects I have been working on.</p>
     ]
 }
 
