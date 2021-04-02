f1d427f6c2b11447c0c6cd513661d7a2549626b9
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-02 09:46:32 +0100

    added about me bio information

--- src/utils/BioArrays.js
+++ src/utils/BioArrays.js
@@ -1,7 +1,24 @@
+import {Link} from "react-router-dom"
+
 const BioArrays = {
     HomeBio: [
         <p className="responsive-text" key="first">My name is <span className="blue-text">Ross McIntyre</span></p>,
         <p className="responsive-text"  key="second">web developer <span className="blue-text">/</span> software engineer</p>
+    ],
+    AboutBio: [
+        <p class="responsive-text">Hello and welcome to my website. My name is <Link to="/" className="blue-text inline-link">
+        Ross McIntyre</Link> and I am a web developer / software engineer.</p>,
+        <p class="responsive-text">
+        I previously worked for many years as a Geotechnical Consultant before discovering a passion for software development and subsequently dedicated
+        my time to retraining, including completing the University of Manchester Full Stack Web Development coding bootcamp.
+        </p>,
+        <p class="responsive-text">
+        I have been employed as a Software Engineer at BJSS Ltd since March 2021.
+        </p>,
+        <p class="responsive-text">
+        My primary expertise is web development using Node.js/Express.js for backend development and React.js for frontend development, with MongoDB or MySQL for databases.  I also have experience with Java and Python. View my <Link to="/portfolio" className="blue-text inline-link">
+        Ross McIntyre</Link> to see the various projects I have been working on.
+        </p>
     ],
     PortfolioBio: [
         <p className="responsive-text"  key="first">My name is <span className="blue-text">Ross McIntyre</span> and I am a web developer and software engineer at BJSS in Manchester</p>,
