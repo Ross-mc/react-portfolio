3c4ed9db531067a3685fee22f503c67f266123b5
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-03 20:54:22 +0100

    added react responsive and set up use media query

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom"
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 
-
-
 const App = () => {
+  const displayFullNav = useMediaQuery({query: '(min-width: 1200px)'});
+
+
   return (
     <>
       <div className="bg-img" />
       <Router>
-        <NavBar />
+        <header>
+         {displayFullNav ? <NavBar /> : <h1>Mobile Nav to go here</h1>}
+        </header>
         <div className ="my-container">
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
