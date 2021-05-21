c800ec2e59c8c02a51f7d36ed65aa089d14d7f60
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-05-21 11:30:30 +0100

    created-no-match-component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import './App.css';
-import './dropdown.css';
+import "./App.css";
+import "./dropdown.css";
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import NavBar from "./components/NavBar";
-import MobileNav from "./components/MobileNav"
+import MobileNav from "./components/MobileNav";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
+import NoMatch from "./components/NoMatch";
 
 const App = () => {
-  const displayFullNav = useMediaQuery({query: '(min-width: 1000px)'});//returns true when the screen is bigger than 1000px
-
+  const displayFullNav = useMediaQuery({ query: "(min-width: 1000px)" }); //returns true when the screen is bigger than 1000px
 
   return (
     <>
       <div className="bg-img" />
       <Router>
-        <header>
-         {displayFullNav ? <NavBar /> : <MobileNav />}
-        </header>
-        <div className ="my-container">
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/portfolio" component={Portfolio} />
+        <header>{displayFullNav ? <NavBar /> : <MobileNav />}</header>
+        <div className="my-container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="*" component={NoMatch} />
+          </Switch>
           <Footer />
         </div>
       </Router>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/NoMatch.js
@@ -0,0 +1,10 @@
+const NoMatch = () => {
+  return (
+    <section className="content" style={{textAlign: "center", color:"whitesmoke"}}>
+      <h1>This is not the page you were looking for</h1>
+      <i class="fab fa-android"></i>
+    </section>
+  )
+}
+
+export default NoMatch
