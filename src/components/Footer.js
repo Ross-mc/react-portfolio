1c929a9f977e47aad0865f62bd7ea3c78529a0bc
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-05-21 11:37:28 +0100

    Social links now open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer className="sticky-footer">
       <div className="socialImages">
-        <a href="https://github.com/Ross-mc">
+        <a href="https://github.com/Ross-mc" target="_blank" rel="noreferrer">
           <i className="fab fa-github fa-2x"></i>
         </a>
-        <a href="https://www.linkedin.com/in/ross-mcintyre-54a13787/">
+        <a href="https://www.linkedin.com/in/ross-mcintyre-54a13787/"  target="_blank" rel="noreferrer">
           <i className="fab fa-linkedin fa-2x"></i>
         </a>
         <a href="mailto:rossmci90@hotmail.co.uk">
